25bc14861c3f9078146fca551a5256304702268a
Elias Sebastian Selenius Haneborg <elias.sebastian.selenius@gmail.com>
2018-11-20 21:37:29 +0100

    Demo project

---
+++ PdfIngDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PdfIng;
+
+namespace PdfIngDemo {
+    class Program {
+        static void Main(string[] args) {
+            var doc = new Document(new MySec());
+            doc.Render();
+            string fileName = "TestOne.pdf";
+            doc.SaveAs(fileName);
+            System.Diagnostics.Process.Start(fileName);
+        }
+    }
+
+    class MySec : Section {
+        protected override void Body() {
+            WriteLine("Hello World this is a test with lots of testing text haha" +
+                 " sounds like texting text, haha its not realy funny i justy need something to talk about");
+
+            WriteLine();
+
+            WriteLine("This is a new paragraph");
+
+            HorizontalMargin = .12;
+
+            string longString = "Hello ";
+            for (int i = 0; i < 10; i++) {
+                longString += longString;
+            }
+
+            WriteLine(longString);
+
+        }
+    }
+}
